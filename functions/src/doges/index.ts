319248ff0f2c6b4c0c049621d521f6c260dcdb3c
Rodrigo <ropi2203@gmail.com>
2018-12-25 17:28:19 -0200

    cadastro de cachorro 1.0

--- functions/src/doges/index.ts
+++ functions/src/doges/index.ts
@@ -23,4 +23,49 @@ export const getTestDog = functions.https.onRequest((request, response) => {
     .catch(error => {
         response.status(500).send("Deu o erro ${error}")
     }) 
-})
+});
+
+export const addDog = functions.https.onRequest((request, response) => {
+    if (request.method !== "POST") {
+        response.status(400).send("Error");
+        // return 0
+    }
+    const age = request.body.age;
+    const gender = request.body.gender;
+    const habits = request.body.habits;
+    const interaction_dogs = request.body.interaction_dogs;
+    const interaction_external = request.body.interaction_external;
+    const interaction_people = request.body.interaction_people;
+    const name = request.body.name;
+    const obs = request.body.obs;
+    const owner = request.body.owner;
+    const photoUrl = request.body.photoUrl;
+    const port = request.body.port;
+    const race = request.body.race;
+    const vet_name = request.body.vet_name;
+    const vet_phone = request.body.vet_phone;
+
+    db.ref('dogs').push().set({
+        age: age,
+        gender: gender,
+        habits: habits,
+        interaction_dogs: interaction_dogs,
+        interaction_external: interaction_external,
+        interaction_people: interaction_people,
+        name: name,
+        obs: obs,
+        owner: owner,
+        photoUrl: photoUrl,
+        port: port,
+        race: race,
+        vet_name: vet_name,
+        vet_phone: vet_phone
+    })
+    .then(() => {
+        response.status(200).send("Cão cadastrado com sucesso");
+    })
+    .catch(function (error) {
+        console.log("Erro cadastrando cachorro:", error);
+        response.status(400).send(error)
+    }); 
+});
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,8 +2,8 @@
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-import {addTestDog,getTestDog} from './doges/index'
-export {addTestDog,getTestDog}
+import {addTestDog,getTestDog, addDog} from './doges/index'
+export {addTestDog,getTestDog, addDog}
 
 import {registerClient} from './clients/index'
 export {registerClient}
