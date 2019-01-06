cc8256a8dbb25f1d687a43b5781a933483aaf07a
Danjfreire <danjfreire@gmail.com>
2019-01-06 15:08:03 -0200

    added function to assign walk

--- functions/src/walks/index.ts
+++ functions/src/walks/index.ts
@@ -19,10 +19,41 @@ export const newWalk = functions.https.onRequest((request, response) =>{
         date : walk.date,
         time : walk.time,
         value : walk.value,
-        walk_type : walk.walk_type
+        walk_type : walk.walk_type,
+        owner_month_year : walk.owner_month_year 
     })
     .then(() => {
         response.status(200).send('Walk addes successfully');
+    })
+    .catch(error => {
+        response.status(400).send(error);
+    })
+})
+
+export const assignWalk = functions.https.onRequest((request, response) =>{
+
+    if(request.method !== 'POST'){
+        response.status(400).send('Error');
+    }
+
+    const walk = request.body.walk;
+    const walker = request.body.walker;
+
+    db.ref('walk_assigned').push().set({
+        address : walk.adress,
+        dog : walk.dog,
+        date : walk.date,
+        time : walk.time,
+        value : walk.value,
+        walk_type : walk.walk_type,
+        owner_month_year : walk.owner_month_year,
+        walker : walker
+    })
+    .then(() => {
+      return db.ref('walk_unassigned/'+ walk.id).remove()
+    })
+    .then(() => {
+        response.status(200).send('the walk was assigned successfully');
     })
     .catch(error => {
         response.status(400).send(error);
